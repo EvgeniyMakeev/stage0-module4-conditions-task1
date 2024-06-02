c8587637d6d30f83b9d950175a5bdf073132f6dc
Evgeniy Makeev <evgeniyvmakeev@gmail.com>
2024-06-02 08:23:39 +0300

    solution

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean cond = false;
+        if (cond) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
